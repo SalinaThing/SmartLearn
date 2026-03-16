df5a2293eca0f0bd12151588e2d2e408d5e477ea
SalinaThing <theengsalina577@gmail.com>
2026-03-16 21:03:17 +0545

    updated config with hostname and portocol

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com, randomuser.me",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
       },
     ],
   },
