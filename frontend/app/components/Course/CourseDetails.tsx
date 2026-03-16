6056f40672ce638db620022311550d8f9f4d1cd6
SalinaThing <theengsalina577@gmail.com>
2026-03-16 23:22:32 +0545

    Updated CourseDetails with Course description along with videourl

--- frontend/app/components/Course/CourseDetails.tsx
+++ frontend/app/components/Course/CourseDetails.tsx
@@ -1,4 +1,8 @@
+import { styles } from '@/app/styles/style';
+import CoursePlayer from '@/app/utils/CoursePlayer';
 import Ratings from '@/app/utils/Ratings';
+import Link from 'next/link';
+import { format } from 'path';
 import React from 'react'
 import { IoCheckmarkDoneCircleOutline } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
@@ -82,7 +86,8 @@ const CourseDetails = ({data}: Props) => {
                             Course Overview
                         </h1>
 
-                        {/* CourseCOntentList */}    
+                        <CourseContentList />   
+                        
                     </div>
                     <br/>
                     <br/>
@@ -113,11 +118,98 @@ const CourseDetails = ({data}: Props) => {
                                 Course Rating ·{data?.reviews?.length}Reviews    
                             </h5>
                         </div>
+                        <br />
+                        {(
+                            data?.reviews && [...data.reviews].reverse()
+                        ).map((item: any, index: number) => (
+
+                        <div className="w-full pb-4" key={index}>
+                            <div className="flex">
+                                <div className="w-[50px] h-[50px]">
+                                    <div className="w-[50px] h-[50px] bg-slate-600 rounded-[50px] flex items-center justify-center cursor-pointer">
+                                        <h1 className="uppercase text-[18px] text-black dark:text-white">
+                                            {item.user.name.slice(0, 2)}
+                                        </h1>
+                                    </div>
+                                </div>
+
+                                <div className="hidden 800px:block pl-2">
+                                    <div className="flex items-center">
+                                        <h5 className="text-[18px] pr-2 text-black dark:text-white">
+                                            {item.user.name}
+                                        </h5>
+                                        <Ratings rating={item.rating} />
+                                    </div>
+
+                                    <p className="text-black dark:text-white">{item.comment}</p>
+
+                                    <small className="text-[#000000d1] dark:text-[#ffffff83]">
+                                    {format(item.createdAt)}
+                                    </small>
+                                </div>
+
+                                <div className="pl-2 flex 800px:hidden items-center">
+                                    <h5 className="text-[18px] pr-2 text-black dark:text-white">
+                                        {item.user.name}
+                                    </h5>
+                                    <Ratings rating={item.rating} />
+                                </div>
+                            </div>
+                        </div>
+                        ))}
+                    </div>   
+                </div>
+                
+                <div className="w-full 800px:w-[35%] relative">
+                    <div className="stickey top-[100px] left-0 z-50 w-full">
+                        <CoursePlayer
+                            videoUrl={data?.demoUrl}
+                            title={data?.title}
+                        />
+
+                        <div className="flex items-center">
+                            <h1 className="pt-5 text-[25px] text-black dark:text-white">
+                                {data.price === 0 ? "Free" : data.price + "Rs."}
+                            </h1>
+
+                            <h5 className="pl-3 text-[20px] mt-2 line-through opacity-80 text-black dark:text-white">
+                                Rs.{data.estimatedPrice}
+                            </h5>
+
+                            <h4 className="pl-5 pt-4 text-[22px] text-black dark:text-white">
+                                {discountPercentagePrice}% Off
+                            </h4>
+                        </div>
+                        
+                        {/* Button(Video Button) */}
+                        <div className="flex items-center">
+                            {isPurchased ? (
+                                <Link
+                                    className={`${styles.button} !w-[180px] my-3 font-Poppins cursor-pointer !bg-[crimson]`}
+                                    href={`/course-access/${data._id}`}
+                                >
+                                    Enter to Course
+                                </Link>
+                            ): (
+                                <div
+                                    className={`${styles.button} !w-[180px] my-3 font-Poppins cursor-pointer !bg-[crimson]`}
+                                    onClick={handleOrder}
+                                >
+                                    Buy Now Rs.{data.price}
+                                </div>
+                            )}
+                        </div>
+                        <br/>
+                        
+                        <p className="pb-1 text-black dark:text-white"> · Source code included</p>
+                        <p className="pb-1 text-black dark:text-white"> · Full lifetime access</p>
+                        <p className="pb-1 text-black dark:text-white"> · Access on mobile and TV</p>
+                        <p className="pb-1 800px:pb-1 text-black dark:text-white"> · Premium Support</p>
 
                     </div>
-                    
                 </div>
-            </div>
+
+            </div>    
         </div>
     </div>
     );
