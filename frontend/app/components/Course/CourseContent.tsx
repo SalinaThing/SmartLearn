dd78ccf8689b6e41b4059b95ac849ba80bcfbb50
SalinaThing <theengsalina577@gmail.com>
2026-03-21 12:42:52 +0545

    Added new component CourseContentMedia and CourseContent for videoContent

--- frontend/app/components/Course/CourseContent.tsx
+++ frontend/app/components/Course/CourseContent.tsx
@@ -1,12 +1,65 @@
-import React from 'react'
+import { useGetCourseContentsQuery } from '@/redux/features/courses/coursesApi';
+import React, { useState } from 'react'
+import Loader from '../Loader/Loader';
+import Heading from '@/app/utils/Heading';
+import CourseContentMedia from './CourseContentMedia';
+import Header from '../Header';
+import CourseContentList from './CourseContentList';
 
 type Props = {
     id:string;
 }
 
 const CourseContent = ({id}: Props) => {
+    const {data: contentData, isLoading} = useGetCourseContentsQuery(id);
+    const [open, setOpen] = useState(false);
+    const [route, setRoute] = useState("Login");
+    const data = contentData?.content;
+
+    const [activeVideo, setActiveVideo]= useState(0);
   return (
-    <div>CourseContent</div>
+    <>
+        {isLoading ? (
+            <Loader/>
+        ):(
+            <>
+                <Header
+                    activeItem={1}
+                    open={open}
+                    setOpen={setOpen}
+                    route={route}
+                    setRoute={setRoute}
+                />
+
+                <div className="w-full grid 800px:grid-cols-10">
+                    <Heading
+                        title={data[activeVideo]?.title}
+                        description="anything"
+                        keywords={data[activeVideo]?.tags}
+                    />
+
+                    <div className="col-span-7">
+                        <CourseContentMedia
+                            data={data}
+                            // setData={setData}
+                            id={id}
+                            activeVideo={activeVideo}
+                            setActiveVideo={setActiveVideo}
+                        />                
+                    </div>
+
+                    <div className="hidden 800px:block 800px:col-span-3">
+                        <CourseContentList
+                            setActiveVideo={setActiveVideo}
+                            data={data}
+                            // setData={setData}
+                            activeVideo={activeVideo}
+                        />
+                    </div>
+                </div>
+            </>
+        )}
+    </>
   )
 }
 
