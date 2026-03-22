664beec962928c7a2888db81e0b29bd07112c79e
SalinaThing <theengsalina577@gmail.com>
2026-03-22 09:36:24 +0545

    Add serverSocket page to add logic for realtime notification

---
+++ backend/serverSocket.js
@@ -0,0 +1,21 @@
+import {Server as SocketIOServer} from "socket.io";
+import http from "http";
+
+export const initSocketServer= (server: http.Server) => {
+    const io = new SocketIOServer(server);
+
+    io.on("connection", (socket) => {
+        console.log("A user is connected!!")
+    })
+
+    //Listen for notification event from the frontend
+    socket.on("notification", (data) => {
+        //broadcast the notification data to all connected clients (teacher dashboard)
+        io.emit("newNotification", data);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("A user is disconnected!!");
+        
+    })
+}
